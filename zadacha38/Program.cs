f90ee74242a95b1e8b214f9e1a90f0acd4e193fc
lavrenoff <lavr020775@gmail.com>
2022-09-02 16:16:30 +0300

    zadacha_38

---
+++ zadacha38/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+void RandomMassive(double[] massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+        massiv[i] = new Random().Next(1, 10); ;
+}
+
+string WriteMassiv(double[] massiv)
+{
+    string str = "";
+
+    for (int i = 0; i < massiv.Length; i++)
+        if (massiv.Length - 1 > i) str = str + Convert.ToString(massiv[i]) + ",";
+        else
+            str = str + Convert.ToString(massiv[i]);
+
+    return "[" + str + "]";
+}
+
+double RazMinMax(double[] massiv)
+{
+    double result=0;
+
+    double min=massiv[0];
+    double max=massiv[0];
+
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        if(min>massiv[i]) min=massiv[i];
+        if(max<massiv[i]) max=massiv[i];
+    }
+
+    result=max-min;
+    return result;
+}
+
+double[] dnumbers=new double[5];
+RandomMassive(dnumbers);
+
+Console.Write($"{WriteMassiv(dnumbers)}=>{RazMinMax(dnumbers)}");
